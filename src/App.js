ddb6573f59c2c463892036bd8e3399437800fc6b
Rafaela <rafalucy3@gmail.com>
2019-12-03 17:16:13 +0000

    Add card and list components to display every single pokemon

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 import NavBar from "./components/layout/NavBar";
+import Dashboard from "./components/layout/Dashboard";
 
 class App extends Component {
   render() {
     return (
-    <div className="App" >
-      <NavBar />
-    </div>
+      <div className="App" >
+        <NavBar />
+          <div className="container">
+            <Dashboard />
+          </div>
+      </div>
     )
   }
 }
