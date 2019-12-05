beb4319b1799c5692691dde523c835ed646b778f
Rafaela <rafalucy3@gmail.com>
2019-12-05 12:18:41 +0000

    fix the issue com duplicate key

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -7,41 +7,44 @@ import InfiniteScroll from 'react-infinite-scroll-component'
 export default class PokemonList extends Component {
     state= {
         pokemon: [],
-        count:0,
+        count: 0,
         limit: 30
     };
 
     componentDidMount() {
+        // this.loadFunc()
         axios
             .get(`https://pokeapi.co/api/v2/pokemon?offset=${this.state.count}&limit=${this.state.limit}`)          
-            .then(res => this.setState({ pokemon: res.data['results']}));
+            .then(res => this.setState({ pokemon: res.data['results'],
+            count: this.state.count + this.state.limit
+        }));
     }
+
 
     loadFunc = () => {
         axios
             .get(`https://pokeapi.co/api/v2/pokemon?offset=${this.state.count}&limit=${this.state.limit}`)          
             .then(res => this.setState({ pokemon: [...this.state.pokemon, ...res.data['results']],
             count: this.state.count + this.state.limit
-            })
+            }),
         );
-
     };
 
     render() {
         return (
-            <React.Fragment>
+            <div>
             <InfiniteScroll
                 dataLength={this.state.pokemon.length}
                 next={this.loadFunc}
                 hasMore={true}
                 loader={<h4>Loading...</h4>}
             >
-            {this.state.pokemon ? (<div className="row">
-                    {this.state.pokemon.map(pokemon => (
-                        <PokemonCard key={pokemon.id} name={pokemon.name} url={pokemon.url} />))}
-                    </div>) : (<h1>Loading Pokemons</h1>)}
+            <div className="row">
+                {this.state.pokemon.map(pokemon => (
+                    <PokemonCard key={pokemon.name} name={pokemon.name} url={pokemon.url} />))}
+            </div>)}
             </InfiniteScroll>
-            </React.Fragment>
+            </div>
         )
     }
 }
