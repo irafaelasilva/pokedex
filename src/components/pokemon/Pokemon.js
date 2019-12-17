afb3d6a365857fb164e38191090926beabbf5f38
Rafaela <rafalucy3@gmail.com>
2019-12-17 16:20:09 +0000

    Add state to create and save abilities

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -2,6 +2,33 @@ import React, { Component } from "react";
 import Axios from "axios";
 
 export default class Pokemon extends Component {
+  state = {
+    name: "",
+    pokemonIndex: "",
+    imageUrl: "",
+    types: [],
+    description: "",
+    statTitleWidth: 3,
+    statBarWidth: 9,
+    stats: {
+      hp: "",
+      attack: "",
+      defense: "",
+      speed: "",
+      specialAttack: "",
+      specialDefense: ""
+    },
+    height: "",
+    weight: "",
+    eggGroups: "",
+    catchRate: "",
+    abilities: "",
+    genderRatioMale: "",
+    genderRatioFemale: "",
+    evs: "",
+    hatchSteps: "",
+    themeColor: "#EF5350"
+  };
   render() {
     return <div></div>;
   }
